8f6d64cc025f6116bb37d450578ad8cde57aee0e
balanikas <c_balanikas@hotmail.com>
2014-09-04 18:35:49 +0200

    fixes in quiztool

---
+++ QuizTool/Importer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ExpressQuiz.Core.Models;
+
+namespace QuizTool
+{
+    class Importer
+    {
+        public Quiz Import(XElement doc)
+        {
+           
+            var quizInfo = doc.Descendants().First(x => x.Attributes().Any(y => y.Value == "with-image-intro"));
+            var quizCat = doc.Descendants().First(x => x.Attributes().Any(y => y.Value == "body")).Attribute("class").Value;
+            var quizName = (quizInfo.FirstNode as XElement).Value;
+            var quizSummary = ((quizInfo.FirstNode as XElement).NextNode as XElement).Value;
+
+
+            var quiz = new Quiz
+            {
+                IsTimeable = true,
+                Name = quizName,
+                Summary = quizSummary,
+                Created = DateTime.Now,
+                CreatedBy = "ChristopherBalanikas",
+                AllowPoints = true,
+                Locked = false,
+                Category = new QuizCategory()
+                {
+                    Name = quizCat
+                }
+            };
+
+
+            var xQuestions = doc.Descendants().Where(x => x.Attributes().Any(y => y.Value.StartsWith("q_and_a_")));
+
+            var qCount = 1;
+            foreach (var q in xQuestions)
+            {
+
+                var qText = q.Descendants().First(x => x.Attributes().Any(y => y.Value.StartsWith("question"))).Value;
+                var correctAnswer = q.Descendants().First(x => x.Attributes("id").Any(y => y.Value == "answer"));
+                var correctAnswerId = int.Parse(correctAnswer.Attribute("data-answer").Value);
+                var tip = correctAnswer.Attribute("data-tip").Value;
+
+
+                var question = new Question();
+                question.Text = qText;
+                question.EstimatedTime = 10;
+                question.OrderId = qCount++;
+                question.Points = 10;
+
+
+                var answers = q.Descendants().Where(x => x.Attributes().Any(y => y.Value.StartsWith("answer block")));
+
+                var aCount = 1;
+                foreach (var a in answers)
+                {
+                    var text = a.Value;
+                    var answer = new Answer();
+                    if (correctAnswerId == aCount)
+                    {
+                        answer.IsCorrect = true;
+                        answer.Explanation = tip;
+                    }
+                    answer.Text = text;
+                    answer.OrderId = aCount++;
+
+                    question.Answers.Add(answer);
+
+                }
+
+                quiz.Questions.Add(question);
+            }
+
+          
+
+            return quiz;
+        }
+
+        public List<Quiz> Import(string source, string destination)
+        {
+
+            var quizzes = new List<Quiz>();
+            var files = Directory.EnumerateFiles(source);
+
+            foreach (var file in files)
+            {
+                var data = Helpers.ReadDocument(file);
+
+                
+
+                try
+                {
+                    var doc = Helpers.HtmlToXElement(data);
+                    var quiz = Import(doc);
+
+                    if (!Helpers.ValidateQuiz(quiz))
+                    {
+                        Trace.WriteLine("quiz is not valid!  " + file);
+                    }
+                    else
+                    {
+                        quizzes.Add(quiz);
+                        Trace.WriteLine("imported from " + file);
+                    }
+
+                  
+                   
+                    
+                }
+                catch (Exception ex)
+                {
+
+                    Trace.WriteLine("failed to import from " + file);
+                    Trace.WriteLine("reason: " + ex.Message);
+                }
+               
+            }
+
+            return quizzes;
+        }
+
+    }
+}
--- QuizTool/Program.cs
+++ QuizTool/Program.cs
@@ -10,6 +10,7 @@ using System.Xml.Serialization;
 using ExpressQuiz.Core.Models;
 using ExpressQuiz.Core.Utils;
 using HtmlAgilityPack;
+using System.Diagnostics;
 
 namespace QuizTool
 {
@@ -17,125 +18,51 @@ namespace QuizTool
     {
         static void Main(string[] args)
         {
-            var outPath = @"C:\Users\grillo\Documents\GitHub\ExpressQuiz\ExpressQuiz\App_Data\out.xml";
 
-            string line;
-            List<Quiz> quizzes = new List<Quiz>();
+            var docPath = Path.Combine(Environment.CurrentDirectory, "documents");
 
-        
-            System.IO.StreamReader file =
-               new System.IO.StreamReader(Path.Combine(Environment.CurrentDirectory,"inputs.txt"));
-            while ((line = file.ReadLine()) != null)
+
+            if (!Directory.Exists(docPath))
             {
-                if (line == "")
-                {
-                    continue;
-                }
-                quizzes.Add(Import(line));
-                Console.WriteLine("imported from " + line);
+                Directory.CreateDirectory(docPath);
             }
 
-            file.Close();
+
+            if (true)
+            {
+                var search = new SmartSearch();
+                var links = search.Find(new Uri("http://www.educationquizzes.com/specialist/"));
+            }
+            
+
+          
+            
+            //save found links to file
+           // Helpers.SaveLinks(Path.Combine(Environment.CurrentDirectory, "links.txt"),links);
+
+
+            //download and save docs 
+           // Helpers.SaveDocuments(Path.Combine(Environment.CurrentDirectory, "documents"), links);
+
+
+            //start importing
+
+            var outPath = Path.Combine(Environment.CurrentDirectory, "out.xml");
+
+
+            var importer = new Importer();
+            var quizzes = importer.Import(docPath, outPath);
+        
+            
 
 
             DataProvider.Export(quizzes, outPath);
-            
-            Console.WriteLine("Done, press something to exit");
-            Console.ReadLine();
+
         
         }
 
 
-        private static Quiz Import(string uri)
-        {
 
-            string data;
-            using (WebClient client = new WebClient())
-            {
-                data = client.DownloadString(uri);
-            }
-
-            var doc = HtmlToXElement(data);
-            
-            var quizInfo = doc.Descendants().First(x => x.Attributes().Any(y => y.Value == "with-image-intro"));
-            var quizCat = doc.Descendants().First(x => x.Attributes().Any(y => y.Value == "body")).Attribute("class").Value;
-            var quizName = (quizInfo.FirstNode as XElement).Value;
-            var quizSummary = ((quizInfo.FirstNode as XElement).NextNode as XElement).Value;
-            var quiz = new Quiz();
-            quiz.IsTimeable = true;
-            quiz.Name = quizName;
-            quiz.Summary = quizSummary;
-            quiz.Created = DateTime.Now;
-            quiz.CreatedBy = "ChristopherBalanikas";
-            quiz.AllowPoints = true;
-            quiz.Locked = false;
-            quiz.Category = new QuizCategory()
-            {
-                Name = quizCat
-            };
-
-
-            var xQuestions = doc.Descendants().Where(x => x.Attributes().Any(y => y.Value.StartsWith("q_and_a_")));
-
-            var qCount = 1;
-            foreach (var q in xQuestions)
-            {
-
-                var qText = q.Descendants().First(x => x.Attributes().Any(y => y.Value.StartsWith("question"))).Value;
-                var correctAnswer = q.Descendants().First(x => x.Attributes("id").Any(y => y.Value == "answer"));
-                var correctAnswerId = int.Parse(correctAnswer.Attribute("data-answer").Value);
-                var tip = correctAnswer.Attribute("data-tip").Value;
-
-
-                var question = new Question();
-                question.Text = qText;
-                question.EstimatedTime = 10;
-                question.OrderId = qCount++;
-                question.Points = 10;
-
-
-                var answers = q.Descendants().Where(x => x.Attributes().Any(y => y.Value.StartsWith("answer block")));
-
-                var aCount = 1;
-                foreach (var a in answers)
-                {
-                    var text = a.Value;
-                    var answer = new Answer();
-                    if (correctAnswerId == aCount)
-                    {
-                        answer.IsCorrect = true;
-                        answer.Explanation = tip;
-                    }
-                    answer.Text = text;
-                    answer.OrderId = aCount++;
-
-                    question.Answers.Add(answer);
-
-                }
-
-                quiz.Questions.Add(question);
-            }
-
-            return quiz;
-
-        }
-
-        public static XElement HtmlToXElement(string html)
-        {
-            if (html == null)
-                throw new ArgumentNullException("html");
-
-            HtmlDocument doc = new HtmlDocument();
-            doc.OptionOutputAsXml = true;
-            doc.LoadHtml(html);
-            using (StringWriter writer = new StringWriter())
-            {
-                doc.Save(writer);
-                using (StringReader reader = new StringReader(writer.ToString()))
-                {
-                    return XElement.Load(reader);
-                }
-            }
-        }
+        
     }
 }
